0d259ea835357f8452d38128b4b740e1bdee67ff
Giorgi1337 <kvacabaya02@gmail.com>
2023-08-12 20:14:15 +0400

    (Bean Lifecycle). Init, Destroy Ð&& Factory

--- src/main/java/GE/spring/ClassicalMusic.java
+++ src/main/java/GE/spring/ClassicalMusic.java
@@ -1,7 +1,16 @@
 package GE.spring;
 
 public class ClassicalMusic implements Music{
-
+    private ClassicalMusic() {}
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+    public void doMyInit() {
+        System.out.println("Initialization");
+    }
+    public void doMyDestroy() {
+        System.out.println("Destruction");
+    }
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
--- src/main/java/GE/spring/TestSpring.java
+++ src/main/java/GE/spring/TestSpring.java
@@ -8,21 +8,22 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-      // Music music = context.getBean("musicBean", Music.class);
-      // MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer",MusicPlayer.class);
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
 
-        boolean comparison = musicPlayer == musicPlayer1;
-        System.out.println(comparison);
-
-        System.out.println(musicPlayer);
-        System.out.println(musicPlayer1);
-
-        musicPlayer1.setVolume(10);
-
-        System.out.println(musicPlayer.getVolume());
-        System.out.println(musicPlayer1.getVolume());
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer",MusicPlayer.class);
+//
+//        boolean comparison = musicPlayer == musicPlayer1;
+//        System.out.println(comparison);
+//
+//        System.out.println(musicPlayer);
+//        System.out.println(musicPlayer1);
+//
+//        musicPlayer1.setVolume(10);
+//
+//        System.out.println(musicPlayer.getVolume());
+//        System.out.println(musicPlayer1.getVolume());
 
         context.close();
     }
